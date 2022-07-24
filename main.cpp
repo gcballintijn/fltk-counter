0da2c70d3581cb75bc714d18f67cde1f5746984d
Gerco Ballintijn <gerco@ballintijn.com>
2022-07-24 13:32:22 +0200

    Made variant with automatic variables instead of unique pointers.

--- main.cpp
+++ main.cpp
@@ -16,12 +16,12 @@
 
 namespace {
     class Handler {
-        std::unique_ptr<Counter> _counter;
-        std::unique_ptr<Fl_Box> _box;
+        Counter & _counter;
+        Fl_Box & _box;
 
     public:
-        Handler(std::unique_ptr<Counter> && counter, std::unique_ptr<Fl_Box> && box)
-            : _counter(std::move(counter)), _box(std::move(box)) {}
+        Handler(Counter & counter, Fl_Box & box)
+            : _counter(counter), _box(box) {}
 
         static void
         down_callback(Fl_Widget * down_button, void * data) {
@@ -38,20 +38,20 @@ namespace {
     private:
         void
         handle_down() {
-            _counter->down();
+            _counter.down();
             update_label();
         }
 
         void
         handle_up() {
-            _counter->up();
+            _counter.up();
             update_label();
         }
 
         void
         update_label() {
-            auto label = std::to_string(_counter->value());
-            _box->label(label.c_str());
+            auto label = std::to_string(_counter.value());
+            _box.label(label.c_str());
         }
     };
 }
@@ -59,19 +59,19 @@ namespace {
 int
 main(int argc, char ** argv) {
 
-    auto counter = std::make_unique<Counter>(42);
+    Counter counter(42);
 
-    auto window = std::make_unique<Fl_Window>(180, 80);
-    auto down_button = std::make_unique<Fl_Button>(0, 0, 60, 80, "down");
-    auto up_button = std::make_unique<Fl_Button>(120, 0, 60, 80, "up");
-    auto box = std::make_unique<Fl_Box>(60, 0, 60, 80, "42");
+    Fl_Window window(180, 80);
+    Fl_Button down_button(0, 0, 60, 80, "down");
+    Fl_Button up_button(120, 0, 60, 80, "up");
+    Fl_Box box(60, 0, 60, 80, "42");
 
-    auto handler = std::make_unique<Handler>(std::move(counter), std::move(box));
-    down_button->callback(Handler::down_callback, handler.get());
-    up_button->callback(Handler::up_callback, handler.get());
+    Handler handler(counter, box);
+    down_button.callback(Handler::down_callback, &handler);
+    up_button.callback(Handler::up_callback, &handler);
 
-    window->end();
-    window->show(argc, argv);
+    window.end();
+    window.show(argc, argv);
 
     return Fl::run();
 }
